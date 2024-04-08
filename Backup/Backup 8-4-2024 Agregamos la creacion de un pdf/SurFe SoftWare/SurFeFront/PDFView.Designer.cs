1b6729930e23e5e9938fd8f1e95a443a9f1d5152
gamex99 <146776124+gamex99@users.noreply.github.com>
2024-04-08 16:54:56 -0300

    backup 8-4-2024

---
+++ Backup/Backup 8-4-2024 Agregamos la creacion de un pdf/SurFe SoftWare/SurFeFront/PDFView.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SurFeFront
+{
+    partial class PDFView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFView));
+            axAcropdf1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)axAcropdf1).BeginInit();
+            SuspendLayout();
+            // 
+            // axAcropdf1
+            // 
+            axAcropdf1.Enabled = true;
+            axAcropdf1.Location = new Point(12, 12);
+            axAcropdf1.Name = "axAcropdf1";
+            axAcropdf1.OcxState = (AxHost.State)resources.GetObject("axAcropdf1.OcxState");
+            axAcropdf1.Size = new Size(776, 426);
+            axAcropdf1.TabIndex = 0;
+            // 
+            // PDFView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(axAcropdf1);
+            Name = "PDFView";
+            Text = "PDFView";
+            Load += PDFView_Load;
+            ((System.ComponentModel.ISupportInitialize)axAcropdf1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF axAcropdf1;
+    }
+}
