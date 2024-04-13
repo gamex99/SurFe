3d05138cef7588c5947bd30235e5f612c0697499
gamex99 <146776124+gamex99@users.noreply.github.com>
2024-04-13 17:28:57 -0300

    11

--- SurFe SoftWare/SurFeDatos/obj/Debug/net6.0/SurFeDatos.AssemblyInfo.cs
+++ SurFe SoftWare/SurFeDatos/obj/Debug/net6.0/SurFeDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurFeDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a66c0cf0df5fc18ef5de2c97ad28cbe00222ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4861d2a0f6e93b85002aef4924fa13e99b64a14")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurFeDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurFeDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
