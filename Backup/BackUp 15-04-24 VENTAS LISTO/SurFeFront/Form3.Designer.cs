fa7eec66dcd5553d9e965790742955dd2811c87a
gamex99 <146776124+gamex99@users.noreply.github.com>
2024-04-15 23:47:16 -0300

    BACKUP

---
+++ Backup/BackUp 15-04-24 VENTAS LISTO/SurFeFront/Form3.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace SurFe
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(22, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 15);
+            label1.TabIndex = 0;
+            label1.Text = "NUEVO PRODUCTO";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DisplayMember = "Categoria";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(177, 24);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.Text = "Categoria";
+            comboBox1.ValueMember = "Categoria";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "BarCode";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(128, 105);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "CodInterno";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(234, 105);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "Producto";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(340, 105);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 5;
+            textBox4.Text = "Precio";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(446, 105);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 6;
+            textBox5.Text = "StockMinimo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(51, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 15);
+            label2.TabIndex = 7;
+            label2.Text = "BarCode";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(131, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Codigo Interno";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(256, 72);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Producto";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(372, 72);
+            label5.Name = "label5";
+            label5.Size = new Size(40, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Precio";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(457, 67);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Stock Minimo";
+            label6.Click += label6_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(411, 192);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 12;
+            button1.Text = "Cargar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(492, 192);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 13;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(416, 144);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(163, 42);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "CargarOtro?";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(57, 18);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(41, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "No";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(11, 18);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(34, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Si";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(591, 227);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Name = "Form3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form3";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+    }
+}
---
+++ Backup/BackUp 15-04-24 VENTAS LISTO/SurFeFront/PDFView.cs
@@ -0,0 +1,35 @@
+﻿using SurFe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SurFeFront
+{
+    public partial class PDFView : Form
+    {
+        string rutaCompletaArchivo;
+        public PDFView(string recibido)
+        {
+            InitializeComponent();
+
+            this.rutaCompletaArchivo = recibido;
+        }
+
+        private void PDFView_Load(object sender, EventArgs e)
+        {
+            //axAcropdf1.src = "C:\\PDF\\elarchivo.pdf";
+            axAcropdf1.src = rutaCompletaArchivo;
+            //tengo que agregar un valor que me traiga desde el form2 para vizualizar el ultimo archivo
+        }
+
+        private void PDFView_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
